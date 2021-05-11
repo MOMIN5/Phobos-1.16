52c718ba7bba3644b2ce22d9cc34e905927647c7
MOMIN5 <drmine05@gmail.com>
2021-05-11 09:35:58 +0530

    bruh

---
+++ src/main/java/me/earth/phobos/Phobos.java
@@ -0,0 +1,12 @@
+package me.earth.phobos;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Phobos implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("My cock is HUGE");
+
+	}
+
+}
